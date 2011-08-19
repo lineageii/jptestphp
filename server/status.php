541087a3f723074bd138e97b1eeb61cfadcdfb77
lineageii <hujiag@gmail.com>
2011-08-19 22:30:04 +0800

    Signed-off-by: lineageii <hujiag@gmail.com>

--- server/status.php
+++ server/status.php
@@ -1,15 +1,28 @@
 <?php
+require_once '../includes.php';
+require_once BASE.'/server/model/Userinfo.php';
+require_once BASE.'/server/model/base/DAO.php';
+
+
 header("Content-type:application/json");	
 
+$ksid = $_COOKIE['ksid'];
+
 //echo '{"canBook":false,"remains":1,"ksTime":"2011年7月3日 下午1点","status":3,"bkkdmc":"大连大学","bkjb":2,"bkkd":23,"retVal":1,"zkz":"11A102040420342"}';
+$userinfoDao = DAO::getDAO('UserinfoDAO');
+$userinfos = $userinfoDao->getUserinfoByKsid($ksid);
+$userinfo = $userinfos[0];
+
+$bkkdmc = $userinfo['wishcity1'];
+$bkjb = $userinfo['wishjb'];
 
 $user = array(
 	'canBook' => false,
 	'remains' => 1,
 	'ksTime' => "2011年12月3日 下午1点",
 	'status' => 2,
-	'bkkdmc' => "大连大学",
-	'bkjb' => 2,
+	'bkkdmc' => $bkkdmc,
+	'bkjb' => $bkjb,
 	'bkkd' => 23,
 	'retVal' => 1,
 	'zkz' => ''
